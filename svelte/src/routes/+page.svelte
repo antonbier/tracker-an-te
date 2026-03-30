b75d26745d882ce2c68076d10349367e64d27b3c
antonbier <antonbier5@gmail.com>
2026-03-30 14:37:10 +0200

    feat(svelte): add root page (navigation router)

---
+++ svelte/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { currentPage } from '$lib/stores.js';
+  import Dashboard from '$lib/components/pages/Dashboard.svelte';
+  import PriceRadar from '$lib/components/pages/PriceRadar.svelte';
+  import MyTrips from '$lib/components/pages/MyTrips.svelte';
+  import Discover from '$lib/components/pages/Discover.svelte';
+</script>
+
+{#if $currentPage === 'home'}
+  <Dashboard />
+{:else if $currentPage === 'priceradar'}
+  <PriceRadar />
+{:else if $currentPage === 'mytrips'}
+  <MyTrips />
+{:else if $currentPage === 'discover'}
+  <Discover />
+{/if}
